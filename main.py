67e4b5e040517cb8670218f6323676bd0988acfa
Fabricio R <frodriguez_cesen@outlook.com>
2018-03-04 16:45:26 -0300

    Adding Random and Simple distributions for IP, MAC and Port

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import random
 from random import shuffle
 import argparse
 
-from scapy.all import *
 from src.data import *
 from src.type import *
 from src.packets import *
@@ -18,7 +17,10 @@ parser = argparse.ArgumentParser(description='BB-gen PCAP generator')
 parser.add_argument('-p', metavar='', help='Type of packet: ipv4, ipv6, vxlan, gre, l2', dest='type', action="store", default='ipv4')
 parser.add_argument('-t', metavar='', help='TCP or UDP', dest='transport', action="store", choices=['tcp', 'udp'], default='tcp')
 parser.add_argument('-n', metavar='', help='Number of entries', dest='num', action="store", type=int, default=10)
-parser.add_argument('--name', metavar='', help='PCAP name', dest='name', action="store", default="test")
+parser.add_argument('-name', metavar='', help='PCAP name', dest='name', action="store", default="test")
+parser.add_argument('--rnip', help='Random IP', dest='rnip', action='store_true', default=False)
+parser.add_argument('--rnmac', help='Random MAC', dest='rnmac', action='store_true', default=False)
+parser.add_argument('--rnport', help='Random Port', dest='rnport', action='store_true', default=False)
 
 parser.add_argument('-A', action='append_const', dest='const_collection',
                     const='value-1-to-append',
@@ -34,6 +36,20 @@ args = parser.parse_args()
 
 entries = args.num
 pname = args.name
+
+if args.rnip == True:
+	ranip = 0
+else:
+	ranip = 1
+if args.rnmac == True:
+	ranmac = 0
+else:
+	ranmac = 1
+if args.rnport == True:
+	ranport = 0
+else:
+	ranport = 1
+
 #Get Protocol type and transport protocol
 e = pkt_type('A')
 e.get_prot_type(args.type)
@@ -41,10 +57,17 @@ e.get_tra_type(args.transport)
 
 #Get IP, MAC and Port list
 f = generator('A')
-f.ip_gen(entries)
-f.mac_gen(entries)
-f.port_gen(entries)
+f.ip_gen(entries,ranip)
+f.mac_gen(entries,ranmac)
+f.port_gen(entries,ranport)
 
 #Create PCAP
 g = create_pkt('A')
 g.pkt_gen(entries, f.macdst, f.macsrc, f.ipdst, f.ipsrc, f.portdst, f.portsrc, e.pktsize, e.prot, e.tra, pname)
+
+print f.ipdst
+print f.ipsrc
+print f.macdst
+print f.macsrc
+print f.portsrc
+print f.portdst
--- src/data.py
+++ src/data.py
@@ -19,8 +19,9 @@ class generator:
 		self.portsrc = []
 		self.portdst = []
 
-
-	def ip_gen(self, entries):
+	#If dist = 0 Generates randorm IP distibution
+	#If dist = 1 Generates simple IP distibution
+	def ip_gen(self, entries, dist):
 		
 		pkts = []
 		
@@ -31,25 +32,28 @@ class generator:
 		u = []
 		ipsrc_c = ""
 		ipdst_c = ""
+		s = 0
 		for m in range(entries):
-			l = 0
-			ipsrc_c = ""
-			ipdst_c = ""
-			for i in range(4):
-				if l == 1:
-					ipdst_c = ipdst_c + "." + str(r[0])
-					ipsrc_c = ipsrc_c + "." + str(r[1])
-					l = 0
-				else:
-					ipdst_c = ipdst_c + str(r[0])
-					ipsrc_c = ipsrc_c + str(r[1])
-				l = l + 1
-				shuffle(r)
+			if s == 0:
+				l = 0
+				ipsrc_c = ""
+				ipdst_c = ""
+				for i in range(4):
+					if l == 1:
+						ipdst_c = ipdst_c + "." + str(r[0])
+						ipsrc_c = ipsrc_c + "." + str(r[1])
+						l = 0
+					else:
+						ipdst_c = ipdst_c + str(r[0])
+						ipsrc_c = ipsrc_c + str(r[1])
+					l = l + 1
+					shuffle(r)
+					if dist == 1:
+						s = 1
 			self.ipdst.append(ipdst_c)
 			self.ipsrc.append(ipsrc_c)
 
-
-	def mac_gen(self, entries):
+	def mac_gen(self, entries, dist):
 	
 		pkts = []
 
@@ -64,42 +68,45 @@ class generator:
 		macdst_c = ""
 		macsrc_hex = ""
 		macdst_hex = ""
+		s = 0
 		for m in range(entries):
-			macsrc_c = "f0:76:1c:"
-			macdst_c = "f0:76:1c:"
-			macsrc_hex = "0xf0:0x76:0x1c:"
-			macdst_hex = "0xf0:0x76:0x1c:"
-			l = 0
-			n = 0
-			for i in range(6):
-				if l == 2:
-					n = 0
-					macsrc_c = macsrc_c + ":" + format(k[0], '01x')
-					macdst_c = macdst_c + ":" + format(k[1], '01x')
-					if n == 0:
-						macsrc_hex = macsrc_hex + ":" + format(k[0], '#01x')
-						macdst_hex = macdst_hex + ":" + format(k[1], '#01x')
-						n = 1
-					l = 0
-				else:
-					macsrc_c = macsrc_c + format(k[0], '01x')
-					macdst_c = macdst_c + format(k[1], '01x')
-					if n == 0:
-						macsrc_hex = macsrc_hex + format(k[0], '#01x')
-						macdst_hex = macdst_hex + format(k[1], '#01x')
-						n = 1
+			if s == 0:
+				macsrc_c = "f0:76:1c:"
+				macdst_c = "f0:76:1c:"
+				macsrc_hex = "0xf0:0x76:0x1c:"
+				macdst_hex = "0xf0:0x76:0x1c:"
+				l = 0
+				n = 0
+				for i in range(6):
+					if l == 2:
+						n = 0
+						macsrc_c = macsrc_c + ":" + format(k[0], '01x')
+						macdst_c = macdst_c + ":" + format(k[1], '01x')
+						if n == 0:
+							macsrc_hex = macsrc_hex + ":" + format(k[0], '#01x')
+							macdst_hex = macdst_hex + ":" + format(k[1], '#01x')
+							n = 1
+						l = 0
 					else:
-						macsrc_hex = macsrc_hex + format(k[0], '01x')
-						macdst_hex = macdst_hex + format(k[1], '01x')
-				l = l + 1
-				shuffle(k)
+						macsrc_c = macsrc_c + format(k[0], '01x')
+						macdst_c = macdst_c + format(k[1], '01x')
+						if n == 0:
+							macsrc_hex = macsrc_hex + format(k[0], '#01x')
+							macdst_hex = macdst_hex + format(k[1], '#01x')
+							n = 1
+						else:
+							macsrc_hex = macsrc_hex + format(k[0], '01x')
+							macdst_hex = macdst_hex + format(k[1], '01x')
+					l = l + 1
+					shuffle(k)
+					if dist == 1:
+						s = 1
 			self.macdst.append(macdst_c)
 			self.macsrc.append(macsrc_c)
 			self.macdst_h.append(macdst_hex)
 			self.macsrc_h.append(macsrc_hex)
 
-
-	def port_gen(self, entries):
+	def port_gen(self, entries, dist):
 		u = []
 		portsrc_c = ""
 		porrdst_c = ""
@@ -112,4 +119,5 @@ class generator:
 			porrdst_c = str(u[1])
 			self.portsrc.append(portsrc_c)
 			self.portdst.append(porrdst_c)
-			shuffle(u)
+			if dist == 0:
+				shuffle(u)
